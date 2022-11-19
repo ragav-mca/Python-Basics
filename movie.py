919529ddf1b20715afafb93e98f538b444559361
ragav-mca <110483081+ragav-mca@users.noreply.github.com>
2022-11-20 03:36:41 +0530

    Delete movie.py

--- movie.py
+++
@@ -1,7 +0,0 @@
-#Get a movie name. Find out how many words are in that movie. 
-
-movie_name = input("Enter the movie name: ")
-print("The movie name is: ", movie_name)
-
-count_words = movie_name.count(" ") + 1
-print("The number of word in the movie are: ", str(count_words))
