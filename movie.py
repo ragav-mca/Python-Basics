c57626bb02bb54f6700d0a6ded06ca3dfc86d90c
ragav-mca <110483081+ragav-mca@users.noreply.github.com>
2022-11-20 13:59:08 +0530

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,7 +1,7 @@
 #Get a movie name. Find out how many words are in that movie. 
 
 movie_name = input("Enter the movie name: ")
-print("The movie name is: ",movie_name)
+print("Movie name: ",movie_name)
 
 word_count = 1
 
@@ -12,3 +12,7 @@ for count in movie_name:
 print("The number of words in the movie are: ", word_count)
 
 
+Output:
+Enter the movie name: The Shawshank Redemption
+Movie name:  The Shawshank Redemption
+The number of words in the movie are:  3
